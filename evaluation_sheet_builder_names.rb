de1e5e00e7ee1dcb731c6afd41b5d0dc16e23e8a
themightyoarfish <rdiederichse@uos.de>
2016-06-19 15:10:43 +0200

    format improvements

--- evaluation_sheet_builder_names.rb
+++ evaluation_sheet_builder_names.rb
@@ -8,12 +8,12 @@ puts "================================================="
 
 #get the data from a JSON file
 if ARGV[0]
-  source_file = File.read(ARGV[0])
-  data = JSON.parse(source_file)
+   source_file = File.read(ARGV[0])
+   data = JSON.parse(source_file)
 else
-  puts "usage: evaluation_sheet.rb <JSON-file>"
-  puts "       (for the correct format see \'example.json\')"
-  exit
+   puts "usage: evaluation_sheet.rb <JSON-file>"
+   puts "       (for the correct format see \'example.json\')"
+   exit
 end
 
 # The data arrays needed from the JSON-File
@@ -36,7 +36,7 @@ puts "Number of groups: #{groups.size}"
 file_name = "#{course}_Blatt_#{sheet_number}.tex"
 File.open(file_name , 'w') do |file|
 
-  file.print '
+   file.print '
   \documentclass[a4paper, 10pt]{scrartcl}
   \usepackage[utf8]{inputenc}
   \usepackage[ngerman]{babel}
@@ -49,15 +49,15 @@ File.open(file_name , 'w') do |file|
   \usepackage{xcolor}
   \geometry{a4paper,left=1cm,right=1cm, top=0.5cm, bottom=0.5cm}
   \pagenumbering{gobble}
-  \begin{document}' + "\n"
+  \begin{document}' + "\n" + '\setlength\tabcolsep{2pt}'
 
-  # generate multiple pages, if you have more than MAX_GROUPS_PER_PAGE
-  pages = groups.size / MAX_GROUPS_PER_PAGE
-  pages = pages - 1 if groups.size % MAX_GROUPS_PER_PAGE == 0
-point_cnt = 0
-  for page in (0..pages)
-    $groups_left = groups.size - (MAX_GROUPS_PER_PAGE * (page + 1)) >= 0 ? 10 : groups.size - MAX_GROUPS_PER_PAGE * page
-#    file.print('\ttfamily \huge{' + course.to_s + '} Testat Blatt ' + sheet_number.to_s)
+   # generate multiple pages, if you have more than MAX_GROUPS_PER_PAGE
+   pages = groups.size / MAX_GROUPS_PER_PAGE
+   pages = pages - 1 if groups.size % MAX_GROUPS_PER_PAGE == 0
+   point_cnt = 0
+   for page in (0..pages)
+      $groups_left = groups.size - (MAX_GROUPS_PER_PAGE * (page + 1)) >= 0 ? 10 : groups.size - MAX_GROUPS_PER_PAGE * page
+      #    file.print('\ttfamily \huge{' + course.to_s + '} Testat Blatt ' + sheet_number.to_s)
 
       #print a little smaller if groups are 2 digits long
       $groups_counter = MAX_GROUPS_PER_PAGE * page
@@ -69,49 +69,44 @@ point_cnt = 0
       file.print "\n" + '& \\textbf{P}'
 
       for i in (1..$groups_left)
-	group = groups[i-1]
-        file.print ' & \textbf{'+ group['name1'][0] +'+' + group['name2'][0]+ '}'
+         group = groups[i-1]
+         file.print ' & \textbf{'+ group['name1'][0] +'+' + group['name2'][0]+ '}'
       end
       file.print ' \\\\ \hline\hline' 
       file.print "\n" + '& '
 
       for i in (1..$groups_left)
-	group = groups[i-1]
-        file.print ' &'+ group['date'][5...8]+ ''
+         group = groups[i-1]
+         file.print ' &'+ '\footnotesize' + group['date'][4...9]+ ''
       end
       file.print ' \\\\ \hline\hline' 
       file.print "\n"
 
 
       for i in (0...exercises.size)
-        file.print '\rowcolor{gray!50}\textbf{Aufgabe ' + (i+1).to_s + ':' + exercises[i]['text'].to_s + '} & ' + exercises[i]['points'].to_s + ("& " * $groups_left) + ' \\\\'
-        file.print '\hline' * 2
-        file.print "\n"
-        for j in (0...exercises[i]['subtasks'].size)
-          subtask = exercises[i]['subtasks'][j]
-          if subtask['points'] == 0
-		file.print subtask['text'].to_s + '& ---' + ('& ---' * $groups_left) + ' \\\\ \hline' + "\n"
-	  else
-		file.print subtask['text'].to_s + '& ' + subtask['points'].to_s + ('& ' * $groups_left) + ' \\\\ \hline' + "\n"
-		point_cnt = point_cnt + subtask['points']
-	  end
-        end
+         file.print '\rowcolor{gray!50}\textbf{Aufgabe ' + (i+1).to_s + ':' + exercises[i]['text'].to_s + '} & ' + exercises[i]['points'].to_s + ("& " * $groups_left) + ' \\\\'
+         file.print '\hline' * 2
+         file.print "\n"
+         for j in (0...exercises[i]['subtasks'].size)
+            subtask = exercises[i]['subtasks'][j]
+            if subtask['points'] == 0
+               file.print subtask['text'].to_s + '& ---' + ('& ---' * $groups_left) + ' \\\\ \hline' + "\n"
+            else
+               file.print subtask['text'].to_s + '& ' + subtask['points'].to_s + ('& ' * $groups_left) + ' \\\\ \hline' + "\n"
+               point_cnt = point_cnt + subtask['points']
+            end
+         end
       end
 
       file.print "\n"
       file.print '\rowcolor{gray!10}\textbf{Summe} & ' + point_cnt.to_s + ('& ' * $groups_left)  + ' \\\\ \hline' + "\n" + '\end{longtable}' + "\n"
 
-      #table of names under the points table
-      names_rows = $groups_left
-      names_columns = 3
-
-     
       if groups.size - (MAX_GROUPS_PER_PAGE * (page + 1)) > 0
-        file.print "
+         file.print "
         \\newpage"
       end
-  end
-  file.print "
+   end
+   file.print "
   \\end{document}"
 end
 
